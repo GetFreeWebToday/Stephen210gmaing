5a0b81f0b355f7b560b8bbb225be88ea72deb51e
Max YT <161175370+MaxYT5184@users.noreply.github.com>
2025-04-28 18:56:54 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("toggle-btn").addEventListener("click", function () {
+    const footerContent = document.getElementById("footer-content");
+    footerContent.style.display = footerContent.style.display === "none" ? "block" : "none";
+});
